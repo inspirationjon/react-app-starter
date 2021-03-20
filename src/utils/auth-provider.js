43d159f506f968da08d1a562ce6c9468b04eb59c
ilhomjon55 <ilhomshamidinov55@gmail.com>
2021-03-20 17:24:34 +0500

    Add extra hooks

---
+++ src/utils/auth-provider.js
@@ -0,0 +1,24 @@
+const localStorageKey = '__auth_provider_token__'
+
+async function getToken() {
+    return window.localStorage.getItem(localStorageKey)
+}
+
+function handleUserResponse({ user }) {
+    window.localStorage.setItem(localStorageKey, user.token)
+    return user
+}
+
+function login({ username, password }) {
+    return client('login', { username, password }).then(handleUserResponse)
+}
+
+function register({ username, password }) {
+    return client('register', { username, password }).then(handleUserResponse)
+}
+
+async function logout() {
+    window.localStorage.removeItem(localStorageKey)
+}
+
+export { getToken, login, register, logout, localStorageKey }
